23bca7ffafd1bbb92172c669b262f638787bf520
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2025-05-21 19:35:32 -0300

    feat: CORs configuration

--- shopinventory-api/src/main/java/com/deyvidsalvatore/shopinventory_api/config/MvcConfig.java
+++ shopinventory-api/src/main/java/com/deyvidsalvatore/shopinventory_api/config/MvcConfig.java
@@ -3,13 +3,27 @@ package com.deyvidsalvatore.shopinventory_api.config;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+	@Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedMethods("*")
+            .allowCredentials(true);
+    }
+    
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		exposeDirectory("user-photos", registry);
