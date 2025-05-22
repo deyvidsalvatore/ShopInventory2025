f66a3c01e9f856b5ebb9d0f1245c08d70f66b6ff
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2025-05-22 10:25:12 -0300

    feat: Container component

--- shopinventory-admin/src/app/modules/dashboard/dashboard.module.ts
+++ shopinventory-admin/src/app/modules/dashboard/dashboard.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { ContainerComponent } from "../../shared/container/container.component";
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardPageComponent
+  ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
-  ]
+    DashboardRoutingModule,
+    ContainerComponent,
+    MatButtonModule
+]
 })
 export class DashboardModule { }
---
+++ shopinventory-admin/src/app/shared/container/container.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.scss',
+  imports: [MatCardModule]
+})
+export class ContainerComponent {
+
+}
